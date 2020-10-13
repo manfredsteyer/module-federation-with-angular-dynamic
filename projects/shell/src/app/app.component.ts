2c295df58e9d1c1cc0521cc54df60308c6fb21c8
Manfred Steyer <manfred.steyer@gmx.net>
2020-10-13 22:34:44 +0200

    make it work with webpack 5

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { buildRoutes } from '../menu-utils';
+import { LookupService } from './microfrontends/lookup.service';
 import { Microfrontend } from './microfrontends/microfrontend';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent /* implements OnInit */ {
+export class AppComponent implements OnInit  {
 
   microfrontends: Microfrontend[] = [];
 
-  // constructor(
-  //   private router: Router,
-  //   private lookupService: LookupService) {
-  // }
+  constructor(
+    private router: Router,
+    private lookupService: LookupService) {
+  }
 
-  // async ngOnInit(): Promise<void> {
-  //   this.microfrontends = await this.lookupService.lookup();
-  //   const routes = buildRoutes(this.microfrontends);
-  //   this.router.resetConfig(routes);
-  // }
+  async ngOnInit(): Promise<void> {
+    this.microfrontends = await this.lookupService.lookup();
+    const routes = buildRoutes(this.microfrontends);
+    this.router.resetConfig(routes);
+  }
 }
 
