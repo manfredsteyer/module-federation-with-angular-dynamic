f2331fcc8f776884f9e1f24ce04bf03e4e419857
Manfred Steyer <manfred.steyer@gmx.net>
2021-12-23 15:44:01 +0100

    add development config

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,1 +1,9 @@
-import('./bootstrap');
+import { loadRemoteEntry } from '@angular-architects/module-federation';
+
+Promise.all([
+    loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:3000/remoteEntry.js'}),
+    loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:3001/remoteEntry.js'})
+])
+.then(() => import('./bootstrap'))
+.catch(err => console.error('error', err));
+
