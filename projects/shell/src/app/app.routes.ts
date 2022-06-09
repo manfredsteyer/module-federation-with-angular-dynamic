c2e72d2f75cc4d6f516cec7b95b28c0b3d81a5c7
Manfred Steyer <manfred.steyer@gmx.net>
2022-06-09 22:50:28 +0200

    add low-level demo

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -3,27 +3,27 @@ import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'flights',
-      loadChildren: () => loadRemoteModule({
-          type: 'manifest',
-          remoteName: 'mfe1',
-          exposedModule: './Module'
-        })
-        .then(m => m.FlightsModule)
-    },
-    {
-      path: 'bookings',
-      loadChildren: () => loadRemoteModule({
-          type: 'manifest',
-          remoteName: 'mfe2',
-          exposedModule: './Module'
-        })
-        .then(m => m.BookingsModule)
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'flights',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module',
+      }).then((m) => m.FlightsModule),
+  },
+  {
+    path: 'bookings',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Module',
+      }).then((m) => m.BookingsModule),
+  },
 ];
--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,17 @@
-import { loadManifest } from '@angular-architects/module-federation';
+import { loadManifest, loadRemoteEntry, setManifest } from '@angular-architects/module-federation';
 
-loadManifest("/assets/mf.manifest.json")
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+// setManifest({
+// 	"mfe1": "http://localhost:4201/remoteEntry.js",
+// 	"mfe2": "http://localhost:4202/remoteEntry.js"
+// });
+
+Promise.all([
+  loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:4201/remoteEntry.js' }),
+  loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:4202/remoteEntry.js' })
+])
+.catch(err => console.error(err))
+.then(_ => import('./bootstrap'))
+.catch(err => console.error(err));
+
+
+// loadManifest("/assets/mf.manifest.json", false)
