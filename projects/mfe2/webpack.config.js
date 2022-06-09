11c685608450a965c89b0a0dc9cbe65edbb7be87
Manfred Steyer <manfred.steyer@gmx.net>
2022-06-09 15:39:49 +0200

    fix: name in webpack.config.js

--- projects/mfe2/webpack.config.js
+++ projects/mfe2/webpack.config.js
@@ -2,7 +2,7 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
 
-  name: 'mfe1',
+  name: 'mfe2',
 
   exposes: {
     // Adjusted line:
